408d316097ac47d241aaf62ef685c67d944ccd28
Haroon Saifi <haroonsaifi2001@gmail.com>
2024-09-06 20:19:26 +0530

    exploring material 3 theming

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -15,6 +15,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(),
     provideAnimationsAsync(),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
   ],
 };
