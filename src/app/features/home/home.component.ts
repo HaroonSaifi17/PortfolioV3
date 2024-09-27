848f5c0f0df9cc55d959bfbe2a4ba4bd7d760b06
Haroon Saifi <haroonsaifi2001@gmail.com>
2024-09-28 01:38:52 +0530

    update

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  title = inject(Title);
+  meta = inject(Meta);
+  constructor() {
+    this.title.setTitle('Portfolio');
 
+    this.meta.addTags([
+      {
+        name: 'description',
+        content:
+          'Full-stack developer portfolio showcasing skills in front-end and back-end development, including projects using Angular, Node.js, and more.',
+      },
+      {
+        name: 'keywords',
+        content:
+          'full-stack developer, portfolio, Angular, Node.js, JavaScript, web development, front-end, back-end, Express, MongoDB, SQL',
+      },
+      { name: 'author', content: 'Mohd Haroon' },
+    ]);
+  }
 }
--- src/app/utils/blog-info.data.ts
+++ src/app/utils/blog-info.data.ts
@@ -2,7 +2,7 @@ import { BlogInfo } from './blog-info.interface';
 
 export const blogInfo: BlogInfo[] = [
   {
-    path: 'navigating-frequent-framework-updates',
+    path: 'frontends-are-changing-too-fast',
     data: {
       title: "Isn't Frontend libraries changing too fast?",
       description:
@@ -16,4 +16,32 @@ export const blogInfo: BlogInfo[] = [
       ],
     },
   },
+  {
+    path: 'tech-companies-are-ruining-our-lives',
+    data: {
+      title: 'Tech companies are ruining our lives',
+      description:
+        'A brief look at the impact of tech companies on our lives and how they fooling us',
+      date: '2024-09-28',
+      keywords: [
+        'Hackethon',
+        'Impact',
+        'Hiring',
+      ],
+    },
+  },
+  {
+    path: 'how-to-setup-zoneless-angular-project',
+    data: {
+      title: 'How to setup zoneless Angular project',
+      description:
+        'A step-by-step guide to setting up a zoneless Angular project',
+      date: '2024-09-29',
+      keywords: [
+        'Angular',
+        'Zoneless',
+        'Project Setup',
+      ],
+    },
+  }
 ];
