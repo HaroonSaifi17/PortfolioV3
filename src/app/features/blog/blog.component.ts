b05d78e7d03e42b97e539742f8ca8d5f36ab69ae
Haroon Saifi <haroonsaifi2001@gmail.com>
2024-09-28 12:18:00 +0530

    update

--- src/app/features/blog/blog.component.ts
+++ src/app/features/blog/blog.component.ts
@@ -2,6 +2,8 @@ import {
   Component,
   ElementRef,
   inject,
+  Renderer2,
+  ViewChild,
   ViewEncapsulation,
 } from '@angular/core';
 import { BlogService } from '../../utils/blog.service';
@@ -9,6 +11,10 @@ import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { Meta, Title } from '@angular/platform-browser';
+import * as Prism from 'prismjs';
+
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-typescript';
 
 @Component({
   selector: 'app-blog',
@@ -26,6 +32,7 @@ export class BlogComponent {
   blogInfo$ = this.ActivatedRoute.data;
   meta = inject(Meta);
   title = inject(Title);
+  renderer = inject(Renderer2);
   constructor() {
     this.ActivatedRoute.url.subscribe((url) => {
       this.blogContent$ = this.BlogService.getBlogContent(url[0].path);
@@ -38,5 +45,33 @@ export class BlogComponent {
         { name: 'author', content: 'Haroon' },
       ]);
     });
+  }
+  ngAfterViewInit(): void {
+    const codeBlocks: HTMLElement[] =
+      this.elementRef.nativeElement.querySelectorAll('pre code');
+    codeBlocks.forEach((codeBlock: HTMLElement) => {
+      Prism.highlightElement(codeBlock);
+      const piCopy = this.renderer.createElement('i');
+      this.renderer.addClass(piCopy, 'pi');
+      this.renderer.addClass(piCopy, 'pi-copy');
+      const button = this.renderer.createElement('a');
+      this.renderer.appendChild(button, piCopy);
+      this.renderer.addClass(button, 'copy-button');
+
+      this.renderer.listen(button, 'click', () => {
+        this.copyToClipboard(codeBlock.innerText);
+        piCopy.classList.remove('pi-copy');
+        piCopy.classList.add('pi-check');
+        setTimeout(() => {
+          piCopy.classList.remove('pi-check');
+          piCopy.classList.add('pi-copy');
+        }, 2000);
+      });
+
+      this.renderer.appendChild(codeBlock.parentElement, button);
+    });
+  }
+  copyToClipboard(code: string) {
+    navigator.clipboard.writeText(code);
   }
 }
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
